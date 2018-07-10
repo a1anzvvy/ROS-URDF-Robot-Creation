24030be4b996c9a4416bccce6a7f567f6ab59349
a1anzvvy <alan.zhangwy@outlook.com>
2018-07-09 20:42:44 -0400

    src upload & first robot

---
+++ catkin_urdf/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/a1anzvvy/Documents/ros_learning/catkin_urdf/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/a1anzvvy/Documents/catkin_ws_latest/devel;/opt/ros/kinetic".split(';') if "/home/a1anzvvy/Documents/catkin_ws_latest/devel;/opt/ros/kinetic" != "" else []
---
+++ catkin_urdf/build/gtest/gtest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/gtest
+# Build directory: /home/a1anzvvy/Documents/ros_learning/catkin_urdf/build/gtest/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
